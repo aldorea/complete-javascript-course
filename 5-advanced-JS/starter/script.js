3955c0aad7a0d78aa9750f549972a01e0a025279
aldorea <adomenechreal@gmail.com>
2019-10-18 13:32:30 +0200

    primitives vs objects

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -1,35 +1,108 @@
-// Function constructor
+// -- Function constructor
 
-var john = {
+// var john = {
+//     name: 'John',
+//     yearOfBirth: 1982,
+//     job: 'teacher'
+// };
+
+// var Person = function (name, yearOfBirth, job) {
+//     this.name = name;
+//     this.yearOfBirth = yearOfBirth;
+//     this.job = job;
+//     // this.calculateAge = function () {
+//     //     console.log(2016 - this.yearOfBirth);
+//     // }
+// };
+
+// Person.prototype.calculateAge = function () {
+//     console.log(2016 - this.yearOfBirth);
+// }
+
+// Person.prototype.lastName = 'Smith';
+
+// var john = new Person('john', 1458, 'teacher');
+// var jane = new Person('Jane', 1897, 'retired');
+
+// john.calculateAge();
+// jane.calculateAge();
+
+// console.log(john.lastName);
+// console.log(jane.lastName);
+
+
+
+// var personProto = {
+//     calculateAge: function () {
+//         console.log(2016 - this.yearofBirth);
+//     }
+// };
+
+// var john = Object.create(personProto);
+// john.name = 'John';
+// john.yearofBirth = 1988;
+// john.job = 'teacher';
+
+// var jane = Object.create(personProto, {
+//     name: { value: 'Jane'},
+//     yearofBirth: { value: 1960 },
+//     job-- Object create
+
+// var personProto = {
+//     calculateAge: function () {
+//         console.log(2016 - this.yearofBirth);
+//     }
+// };
+
+// var john = Object.create(personProto);
+// john.name = 'John';
+// john.yearofBirth = 1988;
+// john.job = 'teacher';
+
+// var jane = Object.create(personProto, {
+//     name: { value: 'Jane'},
+//     yearofBirth: { value: 1960 },
+//     job: { value: 'designer'}
+// });
+
+
+
+// -- Primitives vs objects
+
+// Primitives
+var a = 21;
+var b = a;
+a = 46;
+console.log(a, b);
+
+// Objects
+var obj1 = {
     name: 'John',
-    yearOfBirth: 1982,
-    job: 'teacher'
+    age: 26
 };
 
-var Person = function (name, yearOfBirth, job) {
-    this.name = name;
-    this.yearOfBirth = yearOfBirth;
-    this.job = job;
-    // this.calculateAge = function () {
-    //     console.log(2016 - this.yearOfBirth);
-    // }
+var obj2 = obj1;
+obj1.age = 30;
+console.log(obj1.age);
+console.log(obj2.age);
+
+// Functions
+var age = 27;
+var obj = {
+    name: 'Jonas',
+    city: 'London'
 };
 
-Person.prototype.calculateAge = function () {
-    console.log(2016 - this.yearOfBirth);
+function change(a, b) {
+    a = 30;
+    b.city = 'San Francisco';
 }
 
-Person.prototype.lastName = 'Smith';
+change(age, obj);
+console.log(age);
+console.log(obj.city);
 
-var john = new Person('john', 1458, 'teacher');
-var jane = new Person('Jane', 1897, 'retired');
 
-
-john.calculateAge();
-jane.calculateAge();
-
-console.log(john.lastName);
-console.log(jane.lastName);
 
 
 
